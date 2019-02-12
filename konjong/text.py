2e70f5fb3cead5f803feaccb8ac5d3b8dd60c28c
9H05T343Y <47579514+9H05T343Y@users.noreply.github.com>
2019-02-13 06:56:31 +0700

    Add files via upload

---
+++ konjong/text.py
@@ -0,0 +1,21 @@
+import random
+import sys
+import time
+def type(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+type("Bersabar Adalah kunci Utama dari kehidupan")
+def tutuk(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+tutuk("Haappy Tools!!!")
+def Bucin(s):
+    for c in s +'\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.5)
+Bucin("Bodo amat ah, bacot lo cok")
