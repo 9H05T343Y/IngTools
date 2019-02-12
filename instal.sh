cdf1a435e5ffd874783eddd744d8f4d27d0acffe
9H05T343Y <47579514+9H05T343Y@users.noreply.github.com>
2019-02-13 06:41:43 +0700

    Add files via upload

---
+++ instal.sh
@@ -0,0 +1,11 @@
+figlet -f slant "Menginstall" |lolcat
+
+
+pkg update -y
+pkg upgrade -y
+apt install git -y
+apt install figlet -y
+gem install lolcat -y
+apt install python python -y
+apt install ruby
+bash ping.sh
---
+++ ping.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#Hi..!
+#Kepoo yak ster?
+#kalem cuman simple coding ^^
+#recode?= MEMPERKALUKAN DIRI SENDIRI
+#belajar? pahami syntax dan penempatan code nya
+#sengaja ga gua encrypt / encode biar yang pemula bisa belajar sedikit ^^
+#tau diri aja ^^
+#ctrl+x for quit
+#CODER BY AdnanKhafabi
+
+clear
+date | lolcat
+figlet 9H05T343Y | lolcat
+
+echo	     "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo          "■               Tools Menstabilkan Jaringan                  ■ " |lolcat
+echo 	      "■                        AuthorBy:                           ■ " | lolcat
+echo 	      "■                      Adnan Khafabi                         ■ " | lolcat
+echo 	      "■                   Single itu happy Cuk                     ■ " | lolcat
+echo 	      "■              Dan FriendZone itu menyakitkan                ■ " | lolcat
+echo 	      "■                    Salam PejuangKamu                       ■ " | lolcat
+echo          "■                        Thanks To :                         ■ " |lolcat
+echo          "■                    Wiwi Annisa(TthKu)✅                    ■ " |lolcat
+echo          "■                          Yuniar✅                          ■ " |lolcat
+echo          "■                    Hozinatul Munawaroh🤗                   ■ " |lolcat
+echo	     "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+trap ctrl_c INT
+ctrl_c() {
+clear
+cmatrix
+sleep 1
+exit
+}
+cd output && python text.py
+
+echo      -e "___________________________________________" | lolcat
+echo      -e " Silahkan pilih Ping yang akan di gunakan! " | lolcat
+echo      -e "1.Ping Dengan Server Cloudflare.com        " | lolcat
+echo      -e "2.Ping Dengan Server google.com            " | lolcat
+echo      -e "3.Ping Dengan 1.1.1.1             " | lolcat
+echo      -e "4.Ping Dengan 209.244.0.3         " | lolcat
+echo      -e "5.Ping Dengan 208.67.222.222      " | lolcat
+echo      -e "6.Ping Dengan 74.82.42.42         " | lolcat
+echo      -e "7.Ping Dengan 9.9.9.9 " |lolcat
+echo      -e "0.Exit Program "| lolcat
+echo      -e "___________________________________________" | lolcat
+echo      -e "Pilih Ya Nomorny!!!" |lolcat
+read      -p "Silahkan= " pil
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+figlet -f slant "Mencoba Start"|lolcat
+ping cloudflare.com
+fi
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+figlet -f slant "Mencoba Start"|lolcat
+ping google.com
+fi
+if [ $pil =  ] || [ $pil = 0 ]
+then
+clear
+figlet -f slant SeeGoodBye |lolcat
+exit
+fi
+if [ $pil = 3 ] || [ $pil = 3 ]
+then
+clear
+figlet -f slant "Mencoba Start"|lolcat
+ping 1.1.1.1
+fi
+if [ $pil = 4 ] || [ $pil = 4 ]
+then
+clear
+figlet -f slant "Mencoba Start"|lolcat
+ping 209.244.0.3
+fi
+if [ $pil = 5 ] || [ $pil = 5 ]
+then
+	clear
+figlet -f slant "Mencoba Start"|lolcat
+ping 208.67.222.222
+fi
+if [ $pil = 6 ] || [ $pil = 6]
+then
+	clear
+figlet -f slant "Mencoba Start"|lolcat
+ping 74.82.42.42
+fi
+if [ $pil = 7 ] || [ $pil = 7 ]
+then
+	clear
+figlet -f slant "Mencoba Start"|lolcat
+ping 9.9.9.9
+fi
